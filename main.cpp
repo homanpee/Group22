55b783b9db87c8661bc87b5b97fbe0764648b482
homanpee <mbream18@gmail.com>
2026-01-02 17:27:44 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,19 @@
 #include "header/ComponentAnalyzer.hpp"
 #include "header/CostCalculator.hpp"
 #include "header/Comparator.hpp"
+using namespace std;
+namespace fs = filesystem;
+
+void clearFile(string filename) {
+    ofstream ofs(filename, ios::trunc);
+    ofs.close();}
 
 int main() {
-  
+  string material[] = {"glass","concrete","wood_stave","rubber","copper","cast_iron","galvanized_iron","wrought_iron","stainless_steel","commercial_steel"};
+  for(int i = 0; i<size(material);i++){
+    DataPrep(material[i]);
+    PhysicsEngine();
+    ComponentAnalyzer();
+    CostCalculator();
+  }
 }
