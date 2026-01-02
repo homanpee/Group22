f911e74c7a699e34881bfed0c6657d04dabdda51
homanpee <mbream18@gmail.com>
2026-01-02 15:10:05 +0800

    Update DataPrep.cpp

--- DataPrep.cpp
+++ DataPrep.cpp
@@ -1,6 +1,8 @@
 #include "header/DataPrep.hpp" //don't change this line
+#include <string>
+using namespace std;
 //include any other necessary headers like iostream,cmath ect.
-void DataPrep()//don't change this line
+void DataPrep(string Material)//don't change this line
 {
 //Paste or type code here
 }
