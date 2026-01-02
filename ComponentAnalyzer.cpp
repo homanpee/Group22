7f0b87ffe8463c22b2ceb1033856730ed8b51f6e
homanpee <mbream18@gmail.com>
2026-01-02 16:24:51 +0800

    Update ComponentAnalyzer.cpp

--- ComponentAnalyzer.cpp
+++ ComponentAnalyzer.cpp
@@ -1,4 +1,5 @@
 #include "header/ComponentAnalyzer.hpp" //don't change this line
+#include <fstream>//don't change this line
 //include any other necessary headers like iostream,cmath ect.
 void ComponentAnalyzer()//don't change this line
 {
